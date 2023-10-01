566fc86870edad0cf5ae4954c18533353524aec8
thanhnm777@gmail.com <thanhnm777@gmail.com>
2023-10-01 22:01:07 +0700

    usePara

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,19 @@
 import { useParams } from "react-router-dom";
+import { getCategory } from "../api";
 
 export default function Category() {
-  const {catID} = console.log(useParams());
+  const { catId } = useParams();
+  const category = getCategory(catId);
   return (
     <>
-      <h2>Category Sessions</h2>
-      <p>Category session content displays here...</p>
+      <h2>{category?.name} session</h2>
+      <ul className="session-list">
+        {category?.sessions.map(session => (
+          <li key={session.id} className="session">
+            <p className="session-name"> {session.name}</p>
+            <p>{session.speaker.name} | {session.speaker.org}</p>
+          </li>))}
+      </ul>
     </>
   );
 }
