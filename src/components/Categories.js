fbe4577dd94c4a1ce4ffdb9ebb1c5f490f6b75e1
thanhnm777@gmail.com <thanhnm777@gmail.com>
2023-10-01 22:25:03 +0700

    update navLink

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,20 +1,23 @@
-import {getCategories} from "../api";
-import { Link, Outlet } from "react-router-dom";
+import { getCategories } from "../api";
+import { NavLink, Link, Outlet } from "react-router-dom";
 
 export default function Categories() {
-  const categories= getCategories();
+  //the complicate here meaning that {isActive} Destructuring Objects https://www.w3schools.com/react/react_es6_destructuring.asp
+  let arrowFunction = ( {isActive} ) => ({ color: isActive ? "red" : "blue" })
+  const categories = getCategories();
   return (
     <div className="container">
       <h1>Session Categories</h1>
       <ul className="categories">
-      {/* {categories.map(cat => <li key={cat.id}>{cat.name}</li>)} */}
-      {/* remove key but nothing happen? ah no I see it in console instead */}
-      {categories.map(cat => 
-        <li key={cat.id}>
-        <Link to={cat.id}>{cat.name}</Link>
-        </li>)}
+        {/* {categories.map(cat => <li key={cat.id}>{cat.name}</li>)} */}
+        {/* remove key but nothing happen? ah no I see it in console instead */}
+        {categories.map(cat =>
+          <li key={cat.id}>
+            {/* using navlink give us class="active" */}
+            <NavLink style={arrowFunction} to={cat.id}>{cat.name}</NavLink>
+          </li>)}
       </ul>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
