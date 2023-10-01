34874352b97c17472c1ee96d4b010c15bc0c3525
thanhnm777@gmail.com <thanhnm777@gmail.com>
2023-10-01 20:17:22 +0700

    route

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import logo from "../assets/red30-tech-logo.png";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Categories from "./Categories";
+
 
 function App() {
   return (
@@ -11,7 +15,10 @@ function App() {
           title="Red30 Tech | Home"
         />
       </header>
-
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="categories" element={<Categories/>} />
+      </Routes>
       <footer className="container">
         &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
       </footer>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,17 @@
+import {getCategories} from "../api";
+
+
 export default function Categories() {
+  const categories= getCategories();
   return (
     <div className="container">
       <h1>Session Categories</h1>
+      <ul className="categories">
+      {/* {categories.map(cat => <li key={cat.id}>{cat.name}</li>)} */}
+      {/* remove key but nothing happen? */}
+      {categories.map(cat => <li key={cat.id}>{cat.name}</li>)}
+
+      </ul>
     </div>
   );
 }
